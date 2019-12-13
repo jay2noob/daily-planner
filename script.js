f1eee950fce9e0ecf27cfafc801c7cd7427f21a3
John Fields <jjfields24@outlook.com>
2019-12-12 23:57:57 -0500

    turn in

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("task-7").value = localStorage.getItem("3:00pm");
 document.getElementById("task-8").value = localStorage.getItem("4:00pm");
 document.getElementById("task-9").value = localStorage.getItem("5:00pm");
 
-let currentTime = moment();
+let currentTime = moment().format("HH");
 let dayEl = document.getElementById("day");
 
 function setDay() {
@@ -27,15 +27,15 @@ let time7El = document.getElementById("time-7");
 let time8El = document.getElementById("time-8");
 let time9El = document.getElementById("time-9");
 
-time1El = moment(9, "HH");
-time2El = moment(10, "HH");
-time3El = moment(11, "HH");
-time4El = moment(12, "HH");
-time5El = moment(13, "HH");
-time6El = moment(14, "HH");
-time7El = moment(15, "HH");
-time8El = moment(16, "HH");
-time9El = moment(17, "HH");
+time1El = moment(9, "HH").format("HH");
+time2El = moment(10, "HH").format("HH");
+time3El = moment(11, "HH").format("HH");
+time4El = moment(12, "HH").format("HH");
+time5El = moment(13, "HH").format("HH");
+time6El = moment(14, "HH").format("HH");
+time7El = moment(15, "HH").format("HH");
+time8El = moment(16, "HH").format("HH");
+time9El = moment(17, "HH").format("HH");
 
 let input1 = document.getElementById("task-1");
 let input2 = document.getElementById("task-2");
@@ -48,47 +48,74 @@ let input8 = document.getElementById("task-8");
 let input9 = document.getElementById("task-9");
 
 function setColor() {
-    if (moment(time1El).isBefore(moment(currentTime))) {
+    let currentHour = moment().format("HH");
+    
+    if (currentHour === time1El) {
+        input1.classList.add("red");
+    } else if (moment(time1El).isBefore(moment(currentTime))) {
         input1.classList.add("gray")
     } else {
         input1.classList.add("green")
     }
-    if (moment(time2El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time2El) {
+        input2.classList.add("red");
+    } else if (moment(time2El).isBefore(moment(currentTime))) {
         input2.classList.add("gray")
     } else {
         input2.classList.add("green")
     }
-    if (moment(time3El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time3El) {
+        input3.classList.add("red");
+    } else if (moment(time3El).isBefore(moment(currentTime))) {
         input3.classList.add("gray")
     } else {
         input3.classList.add("green")
     }
-    if (moment(time4El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time4El) {
+        input4.classList.add("red");
+    } else if (moment(time4El).isBefore(moment(currentTime))) {
         input4.classList.add("gray")
     } else {
         input4.classList.add("green")
     }
-    if (moment(time5El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time5El) {
+        input5.classList.add("red");
+    } else if (moment(time5El).isBefore(moment(currentTime))) {
         input5.classList.add("gray")
     } else {
         input5.classList.add("green")
     }
-    if (moment(time6El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time6El) {
+        input6.classList.add("red");
+    } else if (moment(time6El).isBefore(moment(currentTime))) {
         input6.classList.add("gray")
     } else {
         input6.classList.add("green")
     }
-    if (moment(time7El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time7El) {
+        input7.classList.add("red");
+    } else if (moment(time7El).isBefore(moment(currentTime))) {
         input7.classList.add("gray")
     } else {
         input7.classList.add("green")
     }
-    if (moment(time8El).isBefore(moment(currentTime))) {
+
+    if (currentHour === time8El) {
+        input8.classList.add("red");
+    } else if (moment(time8El).isBefore(moment(currentTime))) {
         input8.classList.add("gray")
     } else {
         input8.classList.add("green")
     }
-    if (moment(time9El).isBefore(moment(currentTime))) {
+    if (currentHour === time9El) {
+        input9.classList.add("red");
+    } else if (moment(time9El).isBefore(moment(currentTime))) {
         input9.classList.add("gray")
     } else {
         input9.classList.add("green")
